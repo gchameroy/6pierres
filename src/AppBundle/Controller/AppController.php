e07fbdb439fce76cb41a265bfc063f3f965a270f
Geoffrey Chameroy <geoffrey.chameroy@yahoo.fr>
2016-06-17 15:23:44 +0200

    Add project

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -6,6 +6,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use AppBundle\Form\Type\ProjectInsertType;
+
 class AppController extends Controller
 {
     /**
@@ -28,5 +30,33 @@ class AppController extends Controller
         return $this->render('app/project/list.html.twig', array(
 			'projects' => $projects
 		));
+    }
+
+	/**
+     * @Route("/management/projects/modal/add", name="app_project_modal_add")
+     */
+    public function projectModalAddAction(Request $request)
+    {
+		$project = $this->get('app.project.factory')->create();
+        $form = $this->createForm(ProjectInsertType::class, $project);
+
+        if($form->handleRequest($request)->isValid()){
+			$em = $this->getDoctrine()->getManager();
+			
+			$projects = $em->getRepository('AppBundle:Project')->findAll();
+			foreach($projects As $p){
+				$p->setOrder($p->getOrder() + 1);
+				$em->persist($p);
+			}
+
+            $em->persist($project);
+            $em->flush();
+
+            return $this->redirectToRoute('app_project_list');
+        }
+
+        return $this->render('app/project/modal/add.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 }
---
+++ src/AppBundle/Form/Type/ProjectInsertType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace  AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+class ProjectInsertType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('save', SubmitType::class, array('label' => 'global.action.add.label'))
+        ;
+    }
+
+    public function getParent()
+    {
+        return ProjectType::class;
+    }
+}
